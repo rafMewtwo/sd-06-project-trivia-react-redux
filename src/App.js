ba6c12452af61b6c1dda202b6ba6442c0657fad3
rafMewtwo <ponciano.raf@gmail.com>
2021-01-10 18:45:09 -0300

    complet project

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Login from './Pages/Login';
+import Game from './Pages/Game';
+import Settings from './Pages/Settings';
+import Feedback from './Pages/Feedback';
+import Ranking from './Pages/Ranking';
+
+export default function App() {
+  return (
+    <Switch>
+      <Route exact path="/" component={ Login } />
+      <Route path="/game" component={ Game } />
+      <Route path="/settings" component={ Settings } />
+      <Route path="/feedback" component={ Feedback } />
+      <Route path="/ranking" component={ Ranking } />
+    </Switch>
+  );
+}
---
+++ src/Components/Vagalume.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+class Vagalume extends React.Component {
+  render() {
+    return (
+      <div>
+        <div class="firefly"></div>
+        <div class="firefly"></div>
+        <div class="firefly"></div>
+        <div class="firefly"></div>
+        <div class="firefly"></div>
+        <div class="firefly"></div>
+        <div class="firefly"></div>
+        <div class="firefly"></div>
+        <div class="firefly"></div>
+        <div class="firefly"></div>
+        <div class="firefly"></div>
+        <div class="firefly"></div>
+        <div class="firefly"></div>
+        <div class="firefly"></div>
+        <div class="firefly"></div>
+
+        <div class="firefly"></div>
+      </div>
+
+    );
+  }
+}
+
+export default Vagalume;
